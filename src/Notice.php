bd46d06232a2a1116c76fb511ba1791ed94c542c
Seb Kay <seb@sebkay.com>
2020-06-18 16:29:32 +0100

    Added notice class

---
+++ src/Notice.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace PHPFN;
+
+class Notice
+{
+    private static $session_name = 'notice';
+
+    /**
+     * Set a new notice
+     */
+    public static function set($args)
+    {
+        $_SESSION[self::$session_name] = [
+            'message' => ($args['message'] ?? null),
+            'type'    => ($args['type'] ?? null)
+        ];
+    }
+
+    /**
+     * Get the last notice
+     * 
+     * @return array
+     */
+    public static function get()
+    {
+        $notice = ($_SESSION[self::$session_name] ?? null);
+
+        if (!$notice) {
+            return [];
+        }
+
+        unset($_SESSION[self::$session_name]);
+
+        return $notice;
+    }
+}

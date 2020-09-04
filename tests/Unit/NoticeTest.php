2bd7d31b342de2e4f0bb2ea0ea3fb3d1df829380
Seb Kay <seb@sebkay.com>
2020-09-04 07:38:45 +0100

    Added scaffolding for Unit tests

---
+++ tests/Unit/NoticeTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit;
+
+class NoticeTest extends Test
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    /**
+     * @test
+     */
+    public function test_1()
+    {
+        $this->assertTrue(true);
+    }
+}
+       
